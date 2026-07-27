ded7c8df90231253775e0b20cdfdcdc3350bd6db
Aron Pereira <50774150+pereiraaron@users.noreply.github.com>
2026-07-27 15:20:55 +0530

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findDuplicate(nums) {
+        for (const num of nums) {
+            let index = Math.abs(num) - 1;
+            if (nums[index] < 0) {
+                return Math.abs(num);
+            }
+            nums[index] *= -1;
+        }
+
+        return -1;
+    }
+}
