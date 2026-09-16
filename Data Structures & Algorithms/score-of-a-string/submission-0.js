50101cdd70f1755866af0fe30b77834a9ca423e5
Aron Pereira <50774150+pereiraaron@users.noreply.github.com>
2026-09-16 19:21:29 +0530

    Add: score-of-a-string - submission-0

---
+++ Data Structures & Algorithms/score-of-a-string/submission-0.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    scoreOfString(s) {
+        let score = 0;
+
+        for (let i = 0; i < s.length - 1; i++) {
+            score += Math.abs(s[i].charCodeAt(0) - s[i + 1].charCodeAt(0));
+        }
+
+        return score;
+    }
+}
