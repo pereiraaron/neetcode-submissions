03d9c705f54c525281625272332658a3e617a98c
Aron Pereira <50774150+pereiraaron@users.noreply.github.com>
2026-09-19 15:57:42 +0530

    Add: kth-largest-element-in-an-array - submission-0

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    findKthLargest(nums, k) {
+        let pq = new MinPriorityQueue();
+
+        for (let i = 0; i < nums.length; i++) {
+            pq.enqueue(nums[i]);
+            if (pq.size() > k) {
+                pq.dequeue();
+            }
+        }
+
+        return pq.front();
+    }
+}
